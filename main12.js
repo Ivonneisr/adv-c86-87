21c728b002c202fbd4cd39d006a0cc0f4cf8a23b
Ivonne Sandoval <84480065+Ivonneisr@users.noreply.github.com>
2022-01-25 16:28:51 -0600

    Add files via upload

---
+++ main12.js
@@ -0,0 +1,50 @@
+//Referencia del lienzo y almacenarlo dentro de la variable.
+//var canvas = new fabric.Canvas('myCanvas');
+
+
+//coordenadas iniciales de X y Y para el jugador y almacénalas en las variables
+
+//player_x = 10;
+//player_y = 10;
+
+
+//ancho y alto  de los bloques y almacénalos en las variables
+//block_image_width = 30;
+//block_image_height = 30;
+
+
+//variable para almacenar el objeto de la imagen del jugador.
+//var player_object= "";
+//var block_image_object= "";
+
+
+//función para añadir la imagen del jugador en el lienzo.
+function player_update()
+{
+	//Define la función de fabric.js para subir una imagen al lienzo
+	
+	//almacena un objeto de la imagen dentro de la variable
+	
+}
+
+
+
+
+function new_image(get_image)
+{
+	
+
+}
+
+//window.addEventListener("keydown", my_keydown);
+
+
+
+
+	
+
+
+
+
+
+
